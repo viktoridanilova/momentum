6dc025f3f919fd062559a3e78329fd49f23ec36a
Viktoriya Danilova <v_kozulya@mail.ru>
2022-08-04 16:58:30 +0500

    feat: add weather widget

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -5,6 +5,12 @@ const name = document.querySelector('.name');
 const background = document.querySelector('body');
 const slidePrev = document.querySelector('.slide-prev');
 const slideNext = document.querySelector('.slide-next');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const inputCity = document.querySelector('.city');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
 
 
 
@@ -124,3 +130,38 @@ function setLocalStorage() {
   slidePrev.addEventListener('click', getSlidePrev);
 
   slideNext.addEventListener('click', getSlideNext);
+
+
+  async function getWeather() {  
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&lang=en&appid=4b9833cf58524be6314802104496e329&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json(); 
+
+    weatherIcon.className = 'weather-icon owf';
+
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${Math.floor(data.main.temp)}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+    wind.textContent = `Wind speed: ${Math.floor(data.wind.speed)} m/s`;
+    humidity.textContent = `Humidity: ${data.main.humidity}%`;
+  }
+  getWeather()
+
+  inputCity.addEventListener('keydown', getWeather);
+  
+  inputCity.addEventListener('mouseout', getWeather);
+
+  
+
+  function setLocalStorageCity() {
+    localStorage.setItem('inputCity', inputCity.value);
+  }
+  window.addEventListener('beforeunload', setLocalStorageCity)
+
+  function getLocalStorageCity() {
+    if(localStorage.getItem('inputCity')) {
+      inputCity.value = localStorage.getItem('inputCity');
+    }
+  }
+  window.addEventListener('load', getLocalStorageCity);
+  window.addEventListener('load', getWeather);
