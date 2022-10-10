92957075ee0d66d355bebd46185cd2463003ec32
Viktoriya Danilova <v_kozulya@mail.ru>
2022-10-11 00:38:49 +0500

    feat: add pick progress bar

--- online-zoo/pages/donate/script.js
+++ online-zoo/pages/donate/script.js
@@ -2,7 +2,9 @@ const burgerMenu = document.querySelector('.burger-menu');
 const closeButton = document.querySelector('.burger-menu__button-close');
 const menu = document.querySelector('.burger-menu__wrapper');
 const background = document.querySelector('.wrapper-background');
-
+const moneySelectWrapper = document.querySelector(".progress-bar__money");
+const inputDonate = document.querySelector(".input-summ")
+const selectableInputs = document.querySelectorAll(".progress-bar__money-input")
 burgerMenu.addEventListener('click', () => {
         menu.style.display = 'flex';
         menu.style.top = '0px';
@@ -19,3 +21,18 @@ background.addEventListener('click', () => {
     menu.style.top = '-340px';
     background.style.display = 'none';
 })
+
+moneySelectWrapper.addEventListener("click", setDonateValue)
+
+addEventListener("DOMContentLoaded", () => inputDonate.value = 100)
+
+function setDonateValue() {
+    const selectInputValue = document.querySelector("input:checked").value;
+    inputDonate.value = selectInputValue
+}
+
+inputDonate.addEventListener("input", (event) => setCurrentCheckedValue(event.target.value))
+
+function setCurrentCheckedValue(value) {
+    selectableInputs.forEach(el => el.checked = el.value === value)
+}
