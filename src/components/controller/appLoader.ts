3445eab52d927a4c7997084f5d9990b35c979ecb
Viktoriya Danilova <v_kozulya@mail.ru>
2022-12-11 15:45:50 +0500

    feat: add apiKey

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 export class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'bd3921ba43574f31af73d14b3ec0129b', // получите свой ключ https://newsapi.org/
         });
     }
 }
