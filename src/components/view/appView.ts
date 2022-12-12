f9760bd3da627d951c2146ce95bb60335061f3f7
Viktoriya Danilova <v_kozulya@mail.ru>
2022-12-12 21:01:50 +0500

    refactor: magration appView file to ts

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,21 +1,23 @@
-import News from './news/news';
-import Sources from './sources/sources';
+import { NewsSources, NewsArticles } from '../models';
+import { News } from './news/news';
+import { Sources } from './sources/sources';
 
 export class AppView {
+    private news: News;
+    private sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    public drawNews(data: NewsArticles): void {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    public drawSources(data: NewsSources): void {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
 }
-
-export default AppView;
