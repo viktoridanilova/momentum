6a6bbef94a8d12ed629fae3c68e9b66ba836f847
Viktoriya Danilova <v_kozulya@mail.ru>
2022-12-12 20:56:42 +0500

    refactor: migration appLoader file to ts

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -1,11 +1,7 @@
-import Loader from './loader';
+import { Loader } from './loader';
 
 export class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: 'bd3921ba43574f31af73d14b3ec0129b', // получите свой ключ https://newsapi.org/
-        });
+        super('https://newsapi.org/v2/', { apiKey: 'bd3921ba43574f31af73d14b3ec0129b' });
     }
 }
-
-export default AppLoader;
