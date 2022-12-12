134395ae1143b8d6b239c5d525a3d0abb312d1a3
Viktoriya Danilova <v_kozulya@mail.ru>
2022-12-12 21:09:00 +0500

    refactor: migration app file to ts

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,23 +1,28 @@
-import AppController from '../controller/controller';
+import { NewsArticles, NewsSources } from './../models';
+import { AppController } from '../controller/controller';
 import { AppView } from '../view/appView';
 
 export class App {
     private controller: AppController;
     private view: AppView;
-    private sources: Element | null = document.querySelector(".sources");
+    private sources: Element | null = document.querySelector('.sources');
 
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
-       
     }
 
-    public start() {
+    public start(): void {
         if (this.sources) {
-            this.sources.addEventListener('click', (e: Event) => this.controller.getNews(e, (data: void) => this.view.drawNews(data)));
+            this.sources.addEventListener('click', (e: Event) =>
+                this.controller.getNews(e, (data: NewsArticles | undefined) => {
+                    if (data !== undefined) this.view.drawNews(data);
+                })
+            );
         }
-       
-        this.controller.getSources((data: void) => this.view.drawSources(data));
+        this.controller.getSources((data: NewsSources | undefined) => {
+            if (data !== undefined) this.view.drawSources(data);
+        });
     }
 }
 
